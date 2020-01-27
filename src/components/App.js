2971866cf13b5d6ee14ac40b4716f9ae8a635d96
Mischa Radenovic <miroslav.radenovic@gmail.com>
2020-01-27 14:28:22 -0500

    feat(App): Add data

--- src/components/App.js
+++ src/components/App.js
@@ -17,6 +17,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <div>Icons made by <a href="https://www.flaticon.com/authors/roundicons" title="Roundicons">Roundicons</a> from <a href="https://www.flaticon.com/"     title="Flaticon">www.flaticon.com</a></div>
     </div>
   );
 }
