dec2434f3d84e1c4d87f8560224f7376ec6672fb
Mischa Radenovic <miroslav.radenovic@gmail.com>
2020-01-29 17:20:57 -0500

    feat(QuestionPage): Display Question component

--- src/components/QuestionPage.js
+++ src/components/QuestionPage.js
@@ -1,13 +1,30 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import Question from './Question'
 
 function QuestionPage(props) {
-  console.log(props)
+  const { id, viewType } = props
 
   return (
     <div>
-      Question Page  
+      <Question id={id} viewType={viewType}/>
     </div>  
   )  
 }
 
-export default QuestionPage
+function mapStateToProps({questions /*, authedUser */ }, {match: {params: {question_id : id}}}) {
+  // TODO: Remove const after implementing user authorisation
+  const authedUser = 'sarahedo'
+
+  const question = questions[id]
+  if (!question) return { id, viewType: null}
+  
+  const { optionOne: { votes: one }, optionTwo: { votes: two} } = questions[id]
+  const viewType = one.includes(authedUser) || two.includes(authedUser)
+    ? 'poll'
+    : 'vote'
+
+  return { id, viewType }
+}
+
+export default connect(mapStateToProps)(QuestionPage)
