a7054ce47beea4be090f28a7a00d6728193ce011
Mischa Radenovic <miroslav.radenovic@gmail.com>
2020-01-29 17:14:28 -0500

    fix(App): Move footer into container

--- src/components/App.js
+++ src/components/App.js
@@ -22,20 +22,20 @@ class App extends Component {
           <main>
             <Main />
           </main>
+          <footer>
+            Icons made by{" "}
+            <a
+              href="https://www.flaticon.com/authors/roundicons"
+              title="Roundicons"
+            >
+              Roundicons
+            </a>{" "}
+            from{" "}
+            <a href="https://www.flaticon.com/" title="Flaticon">
+              www.flaticon.com
+            </a>
+          </footer>
         </Container>
-        <footer>
-          Icons made by{" "}
-          <a
-            href="https://www.flaticon.com/authors/roundicons"
-            title="Roundicons"
-          >
-            Roundicons
-          </a>{" "}
-          from{" "}
-          <a href="https://www.flaticon.com/" title="Flaticon">
-            www.flaticon.com
-          </a>
-        </footer>
       </div>
     );
   }
