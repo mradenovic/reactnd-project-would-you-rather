bb695863130b9d1474b5dc67e50112b360519990
Mischa Radenovic <miroslav.radenovic@gmail.com>
2020-02-06 20:31:52 -0500

    feat(async): Set loading in async actions

--- src/actions/async.js
+++ src/actions/async.js
@@ -1,20 +1,25 @@
+import { setLoading } from '../reducers/loading'
 import { getInitialData, saveQuestion } from "../utils/api"
 import { initUsers } from '../reducers/users'
 import { initQuestions } from '../reducers/questions'
 
 export function initData() {
   return dispatch => {
+    dispatch(setLoading(true))
     return getInitialData().then(({ users, questions }) => {
       dispatch(initUsers(users))
       dispatch(initQuestions(questions))
+      dispatch(setLoading(false))
     })
   }
 }
 
 export function addQuestion(question) {
   return dispatch => {
+    dispatch(setLoading(true))
     return saveQuestion(question).then((data) => {
       dispatch(initData())
+      dispatch(setLoading(false))
       return data
     })
   }
