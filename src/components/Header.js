5e3e318262644604fef56f04e4a61d88746e7b0d
Mischa Radenovic <miroslav.radenovic@gmail.com>
2020-02-09 15:46:51 -0500

    feat(Header): Redesign UI

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux'
 import { logOut } from '../reducers/authedUser'
 import HeaderLinks from './HeaderLinks'
 
+import { makeStyles } from '@material-ui/core/styles'
 import { 
   AppBar,
   Toolbar,
@@ -11,23 +12,36 @@ import {
   Typography,
 } from '@material-ui/core'
 
+const useStyles = makeStyles(theme => ({
+  avatar: {
+    padding: theme.spacing(1)
+  },
+  title: {
+    flexGrow: 1,
+  }
+}))
+
 function Header(props) {
+  const classes = useStyles()
   const { user, dispatch } = props
 
   return (
     <AppBar position="static">
       <Toolbar>
-        <Typography variant="h6" style={{ flex: 1}}>
+        <Typography variant="h6" className={classes.title}>
           Would you rather...
         </Typography>
-        {user && 
-          <Button color="inherit" onClick={() => dispatch(logOut())}>
-            Logout {user.name}
-            <Avatar src={user.avatarURL} aria-label={user.name}>
-              {user.name}
-            </Avatar>
-          </Button>
+        <Typography >
+          {user && user.name}
+        </Typography>
+        {user &&
+          <Avatar className={classes.avatar} src={user.avatarURL} aria-label={user.name} />
         }
+        {user && (
+          <Button color="inherit" onClick={() => dispatch(logOut())}>
+            Logout
+          </Button>
+        )}
       </Toolbar>
       
       <HeaderLinks />
