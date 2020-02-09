5179e4a9bc41afb6a22eab55914fd031f32f26c1
Mischa Radenovic <miroslav.radenovic@gmail.com>
2020-02-09 17:17:07 -0500

    feat(QuestionOption): Redesign UI

--- src/components/QuestionOption.js
+++ src/components/QuestionOption.js
@@ -1,29 +1,47 @@
 import React from 'react'
 import { connect } from 'react-redux'
+
+import { makeStyles } from '@material-ui/core/styles'
 import {
   Typography,
-  LinearProgress,
-  Chip } from '@material-ui/core'
+  CircularProgress,
+  Chip, 
+  Badge
+} from '@material-ui/core'
 
+  const useStyles = makeStyles(theme => ({
+    root: {
+      background: props => props.option.votes.includes(props.authedUser)
+        ? 'PowderBlue'
+        : 'inherit'
+    },
+    badge: {
+      position: 'relative',
+      left: theme.spacing(-3),
+      top: theme.spacing(-2.5)
+    },
+  }))
+  
 function QuestionOption(props) {
+  const classes = useStyles(props)
   const { totalVotes, authedUser } = props
   const { text, votes } = props.option
   const votesCount = votes.length
   const voted = votes.includes(authedUser)
   const percentage = Math.floor(votesCount * 100 / totalVotes)
-  const background = voted
-    ? 'PowderBlue'
-    : 'inherit'
 
   return (
-    <div style={{background: background}}>
+    <div className={classes.root}>
       {voted && <Chip color="primary" label="Your vote!" />}  
       <Typography color="textSecondary" variant="h6">
         Would you rather {text}
       </Typography>
-      <LinearProgress variant="determinate" value={percentage} />
+      <div>
+        <CircularProgress variant="static" value={percentage} size={50} thickness={5}/>
+        <Badge className={classes.badge} badgeContent={`${percentage}%`} color="primary" />
+      </div>
       <Typography>
-        {votesCount} out of {totalVotes} vote(s) ({percentage}%)
+        {votesCount} out of {totalVotes} vote(s)
       </Typography>
     </div>
   )
