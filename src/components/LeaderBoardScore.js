2df3852f752a54b185cabcb213179c9739aedd04
Mischa Radenovic <miroslav.radenovic@gmail.com>
2020-02-09 19:41:42 -0500

    feat(LeaderBoard): Redesign UI

--- src/components/LeaderBoardScore.js
+++ src/components/LeaderBoardScore.js
@@ -1,32 +1,75 @@
 import React from 'react'
+
+import { makeStyles } from '@material-ui/core/styles'
 import {
   Card,
   CardContent,
-  CardHeader,
-  Avatar
+  Avatar,
+  Divider,
+  Typography,
 } from '@material-ui/core'
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    padding: theme.spacing(1),
+  },
+  content: {
+    display: 'flex',
+    '& div': {
+      padding: theme.spacing(1),
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center'
+      }
+  },
+  avatar: {
+    width: theme.spacing(10),
+    height: theme.spacing(10)
+  },
+  scores: {
+    flexGrow: 1
+  }
+}))
+
 function LeaderBoardScore(props) {
+  const classes = useStyles()
   const { name, avatarURL, questionCount, answerCount, score} = props.user
 
   return (
-    <div style={{ padding: '10px' }}>
+    <div className={classes.root}>
       <Card >
-        <CardHeader title={name} />
-        <CardContent>
-          <Avatar src={avatarURL} aria-label={name}>
-            {name}
-          </Avatar>
+        {/* <CardHeader title={name} /> */}
+        <CardContent className={classes.content}>
           <div>
-            <div>
-              Questions asked: {questionCount}
-            </div>
-            <div>
-              Questions answered: {answerCount}
-            </div>
+            <Avatar className={classes.avatar} src={avatarURL} aria-label={name}>
+              {name}
+            </Avatar>
           </div>
-
-          Score {score}
+          <div>
+            <Divider orientation="vertical" />
+          </div>
+          <div className={classes.scores}>
+            <Typography variant="h6">
+              {name}
+            </Typography>
+            {/* <CardHeader title={name} /> */}
+            <Typography>
+              Questions asked: {questionCount}
+            </Typography>
+            <Typography>
+              Questions answered: {answerCount}
+            </Typography>
+          </div>
+          <div>
+            <Divider orientation="vertical" />
+          </div>
+          <div className={classes.flexContainer}>
+            <Typography variant="h6">
+              Score
+            </Typography>
+            <Avatar color="primary">{score}</Avatar>
+          </div>
         </CardContent>
       </Card>
     </div>
